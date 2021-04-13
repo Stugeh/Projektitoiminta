b7ce614ccad926f0b41db1cffd1f86dc91a865f1
Stugeh <tuukkaveteli@gmail.com>
2021-04-13 15:33:19 +0300

    cleaned up app.js

--- map-app/src/DesktopView.js
+++ map-app/src/DesktopView.js
@@ -5,21 +5,6 @@ import Map from './components/Map';
 import PictureCard from './components/PictureCard';
 import InfoCard from './components/InfoCard';
 
-// const Lorem = () => (
-//   <div>
-//     <br/>
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-//           Fusce placerat pulvinar quam id suscipit. Nunc erat sem,
-//           mattis in massa ut, posuere consectetur sapien.
-//           Aliquam pharetra odio eu bibendum dapibus.
-//           Fusce sapien nisl, facilisis non rhoncus eget,
-//           tempus nec eros. Donec hendrerit bibendum felis,
-//           quis condimentum mi bibendum vitae.
-//           Nam interdum mauris et laoreet vulputate.
-//           Sed gravida ut odio luctus fringilla.
-//   </div>
-// );
-
 const DesktopView = () => (
   <div className="grid-container">
     <div className="placeHeader">
