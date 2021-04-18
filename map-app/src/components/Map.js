76b7a41c4ba2873381562825ccae18cb7ff7918f
Stugeh <tuukkaveteli@gmail.com>
2021-04-18 13:32:00 +0300

    added markers to the map.

--- map-app/src/components/Map.js
+++ map-app/src/components/Map.js
@@ -2,6 +2,7 @@ const config = require('../utils/config');
 import React from 'react';
 
 import {GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
+import {places} from '../places';
 
 // css style for the div that contains the map.
 const containerStyle = {
@@ -13,7 +14,6 @@ const center = {
   lat: 65.022615,
   lng: 25.471453,
 };
-
 // Map component
 const Map = () => (
   <div className="map">
@@ -24,7 +24,13 @@ const Map = () => (
         zoom={13}
         options={{disableDefaultUI: true}}
       >
-        <Marker position={center}/>
+        {/* .map() is another default array function,
+         that iterates through an array. the property key
+         is always required when you're rendering a list of
+         components iteratively. It needs to be unique.*/}
+        {places.map((place) => (
+          <Marker key={place.name} position={place.coordinates}/>
+        ))}
         <></>
       </GoogleMap>
     </LoadScript>
--- map-app/src/places.js
+++ map-app/src/places.js
@@ -14,7 +14,7 @@ export const places = [
     text: 'Oulun yliopisto, tämä pohjoinen alma mater, on aivan loistava valinta opiskelupaikaksi. Etenki se takia, että se on Oulusa. Oulun yliopistossa on kaheksan tiedekuntaa ja valtavasti eri oppiaineita, mikä tietenki tarkottaa että kavereita riittää. Yliopistolta löytyyki esimerkiksi harrasteporukoita vähän joka lajiin. Oulun yliopiston toiminnot on melko pitkälti yhen kampuksen alla Linnanmaalla, mutta täällä on niin hyvät pyöräbaanat, että kannattee ehottomasti tutkia elämää myös kampuksen ulukopuolella. ',
     coordinates: {
       lat: 65.05977235548156,
-      long: 25.466293498197658,
+      lng: 25.466293498197658,
     },
     challenges: [
       'Kävele lenkki koko kampuksen ympäri',
@@ -28,7 +28,7 @@ export const places = [
     text: '',
     coordinates: {
       lat: 65.06298322512127,
-      long: 25.465259642922696,
+      lng: 25.465259642922696,
     },
     challenges: [],
   },
@@ -38,7 +38,7 @@ export const places = [
     text: '',
     coordinates: {
       lat: 65.06216898080989,
-      long: 25.465774627033518,
+      lng: 25.465774627033518,
     },
     challenges: [],
   },
@@ -48,7 +48,7 @@ export const places = [
     text: '',
     coordinates: {
       lat: 65.01895662086322,
-      long: 25.47805229819547,
+      lng: 25.47805229819547,
     },
     challenges: [],
   },
@@ -58,7 +58,7 @@ export const places = [
     text: '',
     coordinates: {
       lat: 65.01338475886655,
-      long: 25.46458589634565,
+      lng: 25.46458589634565,
     },
     challenges: [],
   },
