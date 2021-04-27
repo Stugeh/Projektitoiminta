546c5f25a967fa8b47ccba83dd695c497e453001
stuge <tuukkaveteli@gmail.com>
2021-04-27 17:54:34 +0300

    challenges now correctly saved and fetched to and from localStorage

--- map-app/src/components/ChallengeElement.js
+++ map-app/src/components/ChallengeElement.js
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   ListItem, ListItemIcon,
   ListItemText, Checkbox} from '@material-ui/core';
 
 const ChallengeElement = ({challenge, setPlaces, activePlace, places}) => {
+  const [checked, setChecked] = useState(challenge.isDone);
+
   const handleToggle = (value) => () => {
-    const updatedChallenge = {...value, isDone: !value.isDone};
-    const updatedChallenges = activePlace.challenges.map((challenge)=>(
-        challenge.text === value.text ? updatedChallenge : challenge
-    ));
-    const newPlace = {...activePlace, challenges: updatedChallenges};
-    const newPlaces = places.map((place) => (
-        place.name === newPlace.name ? newPlace : place
-    ));
+    value.isDone = !value.isDone;
+    const updatedChallenges = updateChallenges(value, activePlace);
+    const newPlaces = updatePlaces(updatedChallenges, activePlace, places);
+    window.localStorage.setItem(
+        'localStoragePlaces', JSON.stringify(newPlaces),
+    );
     setPlaces(newPlaces);
+    setChecked(!checked);
   };
 
   return (
     <ListItem onClick={handleToggle(challenge)}>
       <ListItemIcon>
         <Checkbox
-          checked={challenge.isDone}
-          tabIndex={-1}
+          checked={checked}
         />
       </ListItemIcon>
       <ListItemText id={challenge.text} primary={challenge.text} />
     </ListItem>
   );
+};
+
+
+const updateChallenges= (value, activePlace)=> {
+  const updatedChallenge = {...value, isDone: value.isDone};
+  return activePlace.challenges.map((challenge)=>(
+            challenge.text === value.text ? updatedChallenge : challenge
+  ));
+};
+
+
+const updatePlaces= (updatedChallenges, activePlace, places)=> {
+  const newPlace = {...activePlace, challenges: updatedChallenges};
+  return places.map((place) => (
+            place.name === newPlace.name ? newPlace : place
+  ));
 };
 
 export default ChallengeElement;
