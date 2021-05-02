25c273eadab32662d949e5355e0f4188cfc1e0eb
Miikka Niemi <niemimi@paju.oulu.fi>
2021-05-02 19:13:47 +0300

    Add state hook for showing help window to App.js and pass it down from there

--- map-app/src/components/DesktopView.js
+++ map-app/src/components/DesktopView.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import HelpButton from './HelpButton';
 import Help from './Help';
@@ -6,17 +6,16 @@ import Map from './Map';
 import PictureCard from './PictureCard';
 import InfoCard from './InfoCard';
 
-const DesktopView = ({activePlace, setPlaces, places}) => {
-  const [showHelp, setShowHelp] = useState(true);
-
+const DesktopView = ({showHelp, toggleShowHelp,
+  activePlace, setPlaces, places}) => {
   return (
     <div className="grid-container">
-      <HelpButton onButton={() => setShowHelp(!showHelp)} />
+      <HelpButton onButton={toggleShowHelp} />
       <Help showHelp={showHelp} />
       <div className="placeHeader">
         <h1>Name of the place</h1>
       </div>
-      <Map activateMap={() => setShowHelp(showHelp && !showHelp)} />
+      <Map toggleShowHelp={toggleShowHelp} showHelp={showHelp} />
       <PictureCard />
       <InfoCard activePlace={activePlace}
         setPlaces={setPlaces}
--- map-app/src/components/Map.js
+++ map-app/src/components/Map.js
@@ -15,8 +15,8 @@ const center = {
   lng: 25.471453,
 };
 // Map component
-const Map = ({activateMap}) => (
-  <div className="map" onMouseDown={activateMap}>
+const Map = ({toggleShowHelp, showHelp}) => (
+  <div className="map" onMouseDown={showHelp && toggleShowHelp}>
     <LoadScript googleMapsApiKey={config.MAP_KEY}>
       <GoogleMap
         mapContainerStyle={containerStyle}
