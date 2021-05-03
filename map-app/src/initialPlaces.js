5770cacd39f26fe6994b9d1e48d27eceb7543fda
Miikka Niemi <niemimi@paju.oulu.fi>
2021-05-03 18:14:52 +0300

    Add linebreaks to Yliopisto

--- map-app/src/initialPlaces.js
+++ map-app/src/initialPlaces.js
@@ -11,7 +11,7 @@ export const initialPlaces = [
       './yliopisto1',
       './yliopisto2',
     ],
-    text: 'Oulun yliopisto, tämä pohjoinen alma mater, on aivan loistava valinta opiskelupaikaksi. Etenki se takia, että se on Oulusa. Oulun yliopistossa on kaheksan tiedekuntaa ja valtavasti eri oppiaineita, mikä tietenki tarkottaa että kavereita riittää. Yliopistolta löytyyki esimerkiksi harrasteporukoita vähän joka lajiin. Oulun yliopiston toiminnot on melko pitkälti yhen kampuksen alla Linnanmaalla, mutta täällä on niin hyvät pyöräbaanat, että kannattee ehottomasti tutkia elämää myös kampuksen ulukopuolella. ',
+    text: 'Oulun yliopisto, tämä pohjoinen alma mater, on aivan loistava valinta opiskelupaikaksi. Etenki se takia, että se on Oulusa. Oulun yliopistossa on kaheksan tiedekuntaa ja valtavasti eri oppiaineita, mikä tietenki tarkottaa että kavereita riittää. Yliopistolta löytyyki esimerkiksi harrasteporukoita vähän joka lajiin.\n\nOulun yliopiston toiminnot on melko pitkälti yhen kampuksen alla Linnanmaalla, mutta täällä on niin hyvät pyöräbaanat, että kannattee ehottomasti tutkia elämää myös kampuksen ulukopuolella. ',
     coordinates: {
       lat: 65.05977235548156,
       lng: 25.466293498197658,
