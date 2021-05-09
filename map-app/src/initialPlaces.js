1a984469e87d00ecf4db6d97f70f9489636befb1
Miikka Niemi <niemimi@paju.oulu.fi>
2021-05-09 15:33:51 +0300

    Add newlines and margin to infotexts

--- map-app/src/initialPlaces.js
+++ map-app/src/initialPlaces.js
@@ -11,7 +11,7 @@ export const initialPlaces = [
       './yliopisto1.png',
       './yliopisto2.jpg',
     ],
-    text: 'Oulun yliopisto, tämä pohjoinen alma mater, on aivan loistava valinta opiskelupaikaksi. Etenki se takia, että se on Oulusa. Oulun yliopistossa on kaheksan tiedekuntaa ja valtavasti eri oppiaineita, mikä tietenki tarkottaa että kavereita riittää. Yliopistolta löytyyki esimerkiksi harrasteporukoita vähän joka lajiin.\n\nOulun yliopiston toiminnot on melko pitkälti yhen kampuksen alla Linnanmaalla, mutta täällä on niin hyvät pyöräbaanat, että kannattee ehottomasti tutkia elämää myös kampuksen ulukopuolella. ',
+    text: 'Oulun yliopisto, tämä pohjoinen alma mater, on aivan loistava valinta opiskelupaikaksi. Etenki se takia, että se on Oulusa.\n\nOulun yliopistossa on kaheksan tiedekuntaa ja valtavasti eri oppiaineita, mikä tietenki tarkottaa että kavereita riittää. Yliopistolta löytyyki esimerkiksi harrasteporukoita vähän joka lajiin. Oulun yliopiston toiminnot on melko pitkälti yhen kampuksen alla Linnanmaalla, mutta täällä on niin hyvät pyöräbaanat, että kannattee ehottomasti tutkia elämää myös kampuksen ulukopuolella. ',
     coordinates: {
       lat: 65.05977235548156,
       lng: 25.466293498197658,
@@ -29,7 +29,7 @@ export const initialPlaces = [
       './puutarha_1.jpeg',
       './puutarha_2.jpg',
     ],
-    text: 'Puskeeko stressi pintaan? Deadlinet paukkuu eikä pysty enää keskittymään mihinkään? Läheppä luontoon! Jos ei männikkö innosta, niin onpa onni että Oulun yliopiston välittömässä läheisyydessä on yksi maailman pohjoisimpia tieteellisiä puutarhoja, josta löytyy yli 4000 mitä mielenkiintosempaa lajia. Syksyllä on hyvä kerätä porukka koolle ja lähteä puutarhaan vaikka piknikille!',
+    text: 'Puskeeko stressi pintaan? Deadlinet paukkuu eikä pysty enää keskittymään mihinkään? Läheppä luontoon!\n\nJos ei männikkö innosta, niin onpa onni että Oulun yliopiston välittömässä läheisyydessä on yksi maailman pohjoisimpia tieteellisiä puutarhoja, josta löytyy yli 4000 mitä mielenkiintosempaa lajia. Syksyllä on hyvä kerätä porukka koolle ja lähteä puutarhaan vaikka piknikille!',
     coordinates: {
       lat: 65.06298322512127,
       lng: 25.465259642922696,
@@ -46,7 +46,7 @@ export const initialPlaces = [
       './kyykka_1.jpg',
       './kyykka_2.png',
     ],
-    text: 'Tiiäkkö nää mitä on kyykkä? Tästä ihan läheltä löytyy Kyykkästadion, jossa voi seurata tai kisailla kyykän ja Oulun kansallispelin Mölökyn parissa. Lähteekö Akka, Pappi, Hauki vai Karttu? ',
+    text: 'Tiiäkkö nää mitä on kyykkä?\n\nTästä ihan läheltä löytyy Kyykkästadion, jossa voi seurata tai kisailla kyykän ja Oulun kansallispelin Mölökyn parissa. Lähteekö Akka, Pappi, Hauki vai Karttu? ',
     coordinates: {
       lat: 65.06216898080989,
       lng: 25.465774627033518,
@@ -63,7 +63,7 @@ export const initialPlaces = [
       './ainola_1.jpg',
       './ainola_2.jpg',
     ],
-    text: 'Ainolan puistoon (eli Hupisaariin) kannattee tutustua jo syksyllä, jos on vielä hyviä kelejä. Se on esimerkiksi opiskelijavapun kannalta keskeinen paikka. Sieltä löytyy ihania hengailupaikkoja, joissa voi opiskella rauhassa tai viettää aikaa tutustumalla opiskelukavereihin ja kaupunkiin. Kampuksela on matkaa puistoon joku 6km, mutta pyörällä pääsee tosi hyvin, ja bussilla myös.',
+    text: 'Ainolan puistoon (eli Hupisaariin) kannattee tutustua jo syksyllä, jos on vielä hyviä kelejä. Se on esimerkiksi opiskelijavapun kannalta keskeinen paikka.\n\nSieltä löytyy ihania hengailupaikkoja, joissa voi opiskella rauhassa tai viettää aikaa tutustumalla opiskelukavereihin ja kaupunkiin. Kampukselta on matkaa puistoon joku 6km, mutta pyörällä pääsee tosi hyvin, ja bussilla myös.',
     coordinates: {
       lat: 65.01895662086322,
       lng: 25.47805229819547,
@@ -80,7 +80,7 @@ export const initialPlaces = [
       './torinranta_1.jpg',
       './torinranta_2.jpg',
     ],
-    text: 'Torinranta on vanahaa Oulun parraimmillaan, ja se on myös monien menojen keskittymä. Kesäiltasin Torinrannan laitureilla hengaillee porukkaa ku pipua, ja voi kait siellä talavellaki käyä vaikka kahavilla. Torinranta on ihan keskustan tuntumassa, ja sieltä pääsee mukavasti jatkamaan matkaa vaikka Pikisaareen tai Kuusisaareen. Myös pääkirjasto ja teatteri on Torinrannassa.',
+    text: 'Torinranta on vanahaa Oulun parraimmillaan, ja se on myös monien menojen keskittymä.\n\nKesäiltasin Torinrannan laitureilla hengaillee porukkaa ku pipua, ja voi kait siellä talavellaki käyä vaikka kahavilla. Torinranta on ihan keskustan tuntumassa, ja sieltä pääsee mukavasti jatkamaan matkaa vaikka Pikisaareen tai Kuusisaareen. Myös pääkirjasto ja teatteri on Torinrannassa.',
     coordinates: {
       lat: 65.01338475886655,
       lng: 25.46458589634565,
