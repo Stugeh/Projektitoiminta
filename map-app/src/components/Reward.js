c8280b1bc3db0906055d6de13e6fb0cc9c3b8390
stuge <tuukkaveteli@gmail.com>
2021-05-20 17:12:51 +0300

    fixed css bug where challenges window was narrower than info

--- map-app/src/components/Reward.js
+++ map-app/src/components/Reward.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import useWindowSize from 'react-use/lib/useWindowSize';
 import Confetti from 'react-confetti';
 
 const Reward = () => {
-  const {width, height} = useWindowSize();
   return (
-    <Confetti
-      width={width}
-      height={height}
-    />
+    <Confetti/>
   );
 };
 
